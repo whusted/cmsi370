c20a523682496f0449c6cd712323d5ed6e330b69
whusted <whusted@lion.lmu.edu>
2014-10-24 13:12:20 -0700

    Fixed indents and added all request functions

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -24,6 +24,78 @@ $(function() {
 		            jqXHR.getResponseHeader("Location"));
 		    }
 		});
-        
 	});
+
+	$("#viewCharacters").click(function() {
+	    $.getJSON(
+		    "http://lmu-diabolical.appspot.com/characters",
+		    function (characters) {
+		        // Do something with the character list.
+		        characters.forEach(function (character) {
+		            console.log(character);
+		        });
+		    }
+		);
+	});
+
+	$("#getCharacterById").click(function() {
+		$.getJSON(
+		    "http://lmu-diabolical.appspot.com/characters/" + id,
+		    function (character) {
+		        // Do something with the character.
+		        console.log(character);
+		    }
+		);
+	});
+
+	$("#modifyCharacter").click(function() {
+        $.ajax({
+		    type: 'PUT',
+		    url: "http://lmu-diabolical.appspot.com/characters/" + id,
+		    data: JSON.stringify({
+		        id: id,
+		        name: "Sam",
+		        classType: "rogue",
+		        gender: "MALE",
+		        level: 1,
+		        money: 0
+		    }),
+		    contentType: "application/json",
+		    dataType: "json",
+		    accept: "application/json",
+		    success: function (data, textStatus, jqXHR) {
+		        console.log("Done: no news is good news.");
+		    }
+		});
+	});
+
+	$("#deleteCharacter").click(function(){
+        $.ajax({
+		    type: 'DELETE',
+		    url: "http://lmu-diabolical.appspot.com/characters/" + id,
+		    success: function (data, textStatus, jqXHR) {
+		        console.log("Gone baby gone.");
+		    }
+        });
+	});
+
+	$("#createRandomItem").click(function(){
+        $.getJSON(
+		    "http://lmu-diabolical.appspot.com/items/spawn",
+		    {
+		        level: 50,
+		        slot: "body"
+		    },
+		    function (item) {
+		        // Mmmmm, new item.
+		        console.log(item);
+		    }
+		);
+
+	});
+
+
+
+
+
 });
