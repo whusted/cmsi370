fb8c7e0dd358b9ee0497cda4e672f9b15fcf6bce
whusted <whusted@lion.lmu.edu>
2014-10-27 13:23:03 -0700

    Removed unused code

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -41,18 +41,6 @@ $(function() {
 	    }
 	);
 
-	$("#viewCharacters").click(function() {
-	    $.getJSON(
-		    "http://lmu-diabolical.appspot.com/characters",
-		    function (characters) {
-		        // Do something with the character list.
-		        characters.forEach(function (character) {
-		            console.log(character);
-		        });
-		    }
-		);
-	});
-
 	$("#getCharacterById").click(function() {
 		$.getJSON(
 		    "http://lmu-diabolical.appspot.com/characters/" + id,
