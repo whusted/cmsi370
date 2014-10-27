4521a0df922c5ff399e0c25ccd258a6d6f3b0ad4
whusted <whusted@lion.lmu.edu>
2014-10-27 13:18:39 -0700

    Characters now displayed in table

--- rpg/rpg.js
+++ rpg/rpg.js
@@ -26,6 +26,21 @@ $(function() {
 		});
 	});
 
+    /* Display characters in a table */
+	$.getJSON(
+	    "http://lmu-diabolical.appspot.com/characters",
+	    function (characters) {
+	        $("tbody").append(characters.map(function (character) {
+	            var tr = $(".character-template").clone();
+	            tr.find(".name").text(character.name);
+	            tr.find(".class").text(character.classType);
+	            tr.find(".gender").text(character.gender);
+	            tr.find(".level").text(character.level);
+	            return tr;
+	        }));
+	    }
+	);
+
 	$("#viewCharacters").click(function() {
 	    $.getJSON(
 		    "http://lmu-diabolical.appspot.com/characters",
