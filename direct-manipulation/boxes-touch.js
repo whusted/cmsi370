40c184c365fec24e0acad7ef4961d10625272817
whusted <whusted@lion.lmu.edu>
2014-12-01 13:56:17 -0800

    Dondi bazaar code copied in

---
+++ direct-manipulation/boxes-touch.js
@@ -0,0 +1,86 @@
+var BoxesTouch = {
+    /**
+     * Sets up the given jQuery collection as the drawing area(s).
+     */
+    setDrawingArea: function (jQueryElements) {
+        // Set up any pre-existing box elements for touch behavior.
+        jQueryElements
+            .addClass("drawing-area")
+            
+            // Event handler setup must be low-level because jQuery
+            // doesn't relay touch-specific event properties.
+            .each(function (index, element) {
+                element.addEventListener("touchmove", BoxesTouch.trackDrag, false);
+                element.addEventListener("touchend", BoxesTouch.endDrag, false);
+            })
+
+            .find("div.box").each(function (index, element) {
+                element.addEventListener("touchstart", BoxesTouch.startMove, false);
+                element.addEventListener("touchend", BoxesTouch.unhighlight, false);
+            });
+    },
+
+    /**
+     * Tracks a box as it is rubberbanded or moved across the drawing area.
+     */
+    trackDrag: function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            // Don't bother if we aren't tracking anything.
+            if (touch.target.movingBox) {
+                // Reposition the object.
+                touch.target.movingBox.offset({
+                    left: touch.pageX - touch.target.deltaX,
+                    top: touch.pageY - touch.target.deltaY
+                });
+            }
+        });
+        
+        // Don't do any touch scrolling.
+        event.preventDefault();
+    },
+
+    /**
+     * Concludes a drawing or moving sequence.
+     */
+    endDrag: function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            if (touch.target.movingBox) {
+                // Change state to "not-moving-anything" by clearing out
+                // touch.target.movingBox.
+                touch.target.movingBox = null;
+            }
+        });
+    },
+
+    /**
+     * Indicates that an element is unhighlighted.
+     */
+    unhighlight: function () {
+        $(this).removeClass("box-highlight");
+    },
+
+    /**
+     * Begins a box move sequence.
+     */
+    startMove: function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            // Highlight the element.
+            $(touch.target).addClass("box-highlight");
+
+            // Take note of the box's current (global) location.
+            var jThis = $(touch.target),
+                startOffset = jThis.offset();
+
+            // Set the drawing area's state to indicate that it is
+            // in the middle of a move.
+            touch.target.movingBox = jThis;
+            touch.target.deltaX = touch.pageX - startOffset.left;
+            touch.target.deltaY = touch.pageY - startOffset.top;
+        });
+
+        // Eat up the event so that the drawing area does not
+        // deal with it.
+        event.stopPropagation();
+    }
+
+};
