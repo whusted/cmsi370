6239acac4c72de6b46f2795af64832a03e3a74d6
whusted <whusted@lion.lmu.edu>
2014-12-04 23:16:47 -0800

    Slider button moving now

--- widget-from-scratch/widget.js
+++ widget-from-scratch/widget.js
@@ -1,20 +1,55 @@
 $(function() {
+    var LEFT_BUTTON = 1;
   window.Slider = {
+
         setSliderButton: function (jQueryElements) {
           var BUTTON_TEMPLATE = "<div class='slide-button' id='slide-button'></div>";
           jQueryElements
             .addClass("slide")
             .append(BUTTON_TEMPLATE)
             .mousedown(this.trackDrag)
+            .mousemove(this.moveSlider)
             
             // We conclude drawing on either a mouseup or a mouseleave.
-            .mouseup(this.endDrag)
-            .mouseleave(this.endDrag);
+            .mouseup(this.endDrag);
         },
 
         trackDrag: function (event) {
-          
+          if (event.which === LEFT_BUTTON) {
+              // Take note of the box's current (global) location.
+              var deltaX,
+                  deltaY,
+                  startOffset;
+              if (event.target.id === "slide-button") {
+                  $("#slider").mousemove(this.moveSlider);
+                  startOffset = $("#slide-button").offset();
+                  this.deltaX = event.pageX - startOffset.left;
+                  this.deltaY = event.pageY - startOffset.top;
+                  console.log(deltaX);
+                  console.log(deltaY);
+
+                  // Eat up the event so that the drawing area does not
+                  // deal with it.
+                  event.stopPropagation();
+
+              }
+              
+              
+          }
+        },
+
+        endDrag: function (event) {
+          console.log("Done dragging");
+        },
+
+        moveSlider: function (event) {
+          console.log("moving");
+          $("#slide-button").offset({
+            left: event.pageX - this.deltaX
+          });
+          // We only move using the left mouse button.
         }
+          
 	}
 
 });
