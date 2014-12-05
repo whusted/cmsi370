301bf4550158d9236d323fe8144a76c2db67c61b
whusted <whusted@lion.lmu.edu>
2014-12-04 21:25:11 -0800

    Refactor to plan ahead for jquery widget

--- widget-from-scratch/widget.js
+++ widget-from-scratch/widget.js
@@ -1,9 +1,20 @@
 $(function() {
   window.Slider = {
         setSliderButton: function (jQueryElements) {
+          var BUTTON_TEMPLATE = "<div class='slide-button' id='slide-button'></div>";
+          jQueryElements
+            .addClass("slide")
+            .append(BUTTON_TEMPLATE)
+            .mousedown(this.trackDrag)
+            
+            // We conclude drawing on either a mouseup or a mouseleave.
+            .mouseup(this.endDrag)
+            .mouseleave(this.endDrag);
+        },
 
+        trackDrag: function (event) {
+          
         }
 	}
-
 
 });
