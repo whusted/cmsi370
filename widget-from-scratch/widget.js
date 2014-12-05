115287bfb090951ca148f79459f29b4df2978028
whusted <whusted@lion.lmu.edu>
2014-12-05 00:20:57 -0800

    Slider displays value now

--- widget-from-scratch/widget.js
+++ widget-from-scratch/widget.js
@@ -1,6 +1,6 @@
 $(function() {
     var LEFT_BUTTON = 1;
-    var SLIDER_WIDTH = 450;
+    var SLIDER_WIDTH = 500;
   window.Slider = {
 
         setSliderButton: function (jQueryElements) {
@@ -10,10 +10,7 @@ $(function() {
             .css("width", SLIDER_WIDTH + "px")
             .append(BUTTON_TEMPLATE)
             .mousedown(this.trackDrag)
-            .mousemove(this.moveSlider)
-            
-            // We conclude drawing on either a mouseup or a mouseleave.
-            .mouseup(this.endDrag);
+            .mousemove(this.moveSlider);
         },
 
         trackDrag: function (event) {
@@ -34,20 +31,23 @@ $(function() {
           }
         },
 
-        endDrag: function (event) {
-          console.log("Done dragging");
-        },
-
         moveSlider: function (event) {
           var sliderLeft = $("#slider").offset().left;
+          var INCREMENT_BY = SLIDER_WIDTH / 100;
           if (event.which === LEFT_BUTTON) {
             // Keep slider button on slider
             if (event.pageX < sliderLeft + SLIDER_WIDTH && event.pageX > sliderLeft) {
               $("#slide-button").offset({
                 left: event.pageX - this.deltaX
               });
+              this.sliderValue = Math.floor( ($("#slide-button").offset().left - sliderLeft) / INCREMENT_BY) + 4; // Ugh bad way to make up for error
+              $("#slide-button").text(this.sliderValue);
             }
           }
+        },
+
+        getValue: function () {
+          return this.sliderValue;
         }
           
 	}
